1d33a189f2d29dc5d41d333cb8e9cc0b8db9a4e8
Russell Prince <rsprince@mtu.edu>
2017-05-15 21:08:29 -0500

    Add support for Tiva C series Microcontrollers

---
+++ tiva/build/main.d
@@ -0,0 +1,16 @@
+build/main.o: src/main.c \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_types.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_gpio.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_memmap.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_sysctl.h \
+ /home/evil_linux_box/projects/tivaware/driverlib/gpio.h \
+ /home/evil_linux_box/projects/tivaware/driverlib/rom.h \
+ /home/evil_linux_box/projects/tivaware/driverlib/sysctl.h \
+ /home/evil_linux_box/projects/tivaware/driverlib/pin_map.h \
+ /home/evil_linux_box/projects/tivaware/driverlib/can.h
---
+++ tiva/build/startup_gcc.d
@@ -0,0 +1,9 @@
+build/startup_gcc.o: src/startup_gcc.c \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_nvic.h \
+ /home/evil_linux_box/projects/tivaware/inc/hw_types.h
